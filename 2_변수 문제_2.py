1c4be9fff377a8d33d5527640092f5a9e741bf85
gsangf10 <98445860+gsangf10@users.noreply.github.com>
2022-03-20 16:58:37 +0900

    Create 2_변수 문제_2.py

---
+++ 2_변수 문제_2.py
@@ -0,0 +1,28 @@
+#아래 코드를 변수를 이용해 간단하게 바꿔주세요.
+
+print(1)
+
+print(1 + 2)
+
+print(1 + 2 + 3)
+
+print(1 + 2 + 3 + 4)
+
+print(1 + 2 + 3 + 4 + 5)
+
+print("==== 변환 ====")
+
+a = 1
+print(a)
+
+a = a + 2
+print(a)
+
+a = a + 3
+print(a)
+
+a = a + 4
+print(a)
+
+a = a + 5
+print(a)
