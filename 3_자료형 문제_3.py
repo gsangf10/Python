981efe8a53e70b7a17e195eb9f151c0e64b508db
gsangf10 <98445860+gsangf10@users.noreply.github.com>
2022-03-21 18:56:47 +0900

    Create 3_자료형 문제_3.py

---
+++ 3_자료형 문제_3.py
@@ -0,0 +1,40 @@
+# 다음 명제를 비교연산자를 이용해 직접 표현해보고 print로 그 결과를 확인해보세요.
+
+print(True) # 참
+print(False) # 거짓
+
+# 문제시작
+print("=== 문제 ===")
+a = 10
+
+# a와 10이 같다.
+print(a == 10) # True
+
+# a와 10이 같지 않다.
+print(a != 10) # False
+
+# a가 10보다 크다.
+print(a > 10) # False
+
+# a가 10보다 크거나 같다.
+print(a >= 10) # True
+
+
+b = 10
+
+# a와 b가 같다.
+print(a == b) # True
+
+# a와 b가 같지 않다.
+print(a != b) # False
+
+c = False
+
+# c와 False가 같다.
+print(c == False) # True
+
+
+d = True
+
+# c와 d가 같다.
+print(c == d) # False
