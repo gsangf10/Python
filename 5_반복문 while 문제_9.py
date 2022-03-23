40bd5515a0f90064d316906f4e1f4d8c5c1a69d2
gsangf10 <98445860+gsangf10@users.noreply.github.com>
2022-03-23 19:12:03 +0900

    Create 5_반복문 while 문제_9.py

---
+++ 5_반복문 while 문제_9.py
@@ -0,0 +1,15 @@
+# 구구단 만들기
+
+# 3단
+
+# print("{} X 1 = {}".format(dan, dan * 1))
+# print("{} X 2 = {}".format(dan, dan * 2))
+# print("{} X 3 = {}".format(dan, dan * 3))
+# print("{} X 4 = {}".format(dan, dan * 4))
+dan = 5
+i = 1
+while i <= 9 :
+    #======
+    print("{} X {} = {}".format(dan, i, dan * i))
+    #======
+    i += 1
